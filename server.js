75a03fae15c2e7aaa4b4ad8bbbf58a5e348f84b6
Roman Brito <romanbrito1@gmail.com>
2017-12-29 16:14:37 -0600

    graphql server

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 import express from 'express';
-
+import graphqlHTTP from 'express-graphql';
+import schema from './schema';
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('GraphQL and Relay modern are cool!!');
 });
+
+const root = {hello: () => "Hi, I'm Manny"};
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true, //gui
+}));
 
 app.listen(8080, () => console.log('Running server on localhost:8080/graphql'));
