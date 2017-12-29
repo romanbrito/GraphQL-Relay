4e02e113d74b391a05820c8d502033f67043cd01
Roman Brito <romanbrito1@gmail.com>
2017-12-29 16:37:01 -0600

    a more complex schema

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
 import schema from './schema';
+
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('GraphQL and Relay modern are cool!!');
 });
 
-const root = {hello: () => "Hi, I'm Manny"};
+const root = {
+  friend: () => {
+    return {
+      "id": 123213132,
+      "firstName": "Roman",
+      "lastName": "Brito",
+      "gender": "Male",
+      "language": "English",
+      "email": "romanbrito1@gmail.com"
+    }
+  }
+};
 
 app.use('/graphql', graphqlHTTP({
   schema: schema,
