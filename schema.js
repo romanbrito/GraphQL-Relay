a14831200905a9f7ce3ca31097dbd623e0fa7e34
Roman Brito <romanbrito1@gmail.com>
2017-12-29 22:54:31 -0600

    updated package.json for dependencies to work

--- schema.js
+++ schema.js
@@ -1,38 +1,32 @@
-import {buildSchema} from 'graphql';
+import { buildSchema } from 'graphql';
 
 const schema = buildSchema(`
-input FriendInput
-{
-  id: ID
-  firstName: String
-  lastName: String
-  gender: String
-  language: String
-  email: String
-}
+    input FriendInput {
+        id: ID
+        firstName: String
+        lastName: String
+        gender: String
+        language: String
+        email: String
+    }
+    
+    type Friend {
+        id: ID
+        firstName: String
+        lastName: String
+        gender: String
+        language: String
+        email: String
+    }
+    
+    type Query {
+        getFriend(id: ID!): Friend
+    }
 
-type Friend
-{
-  id: ID
-  firstName: String
-  lastName: String
-  gender: String
-  language: String
-  email: String
-}
-
-type Query
-{
-  getFriend(id: ID!): Friend
-}
-
-type Mutation {
-  createFriend(input: FriendInput): Friend
-  updateFriend(id: ID!, input: FriendInput): Friend
-}
-`
-);
-
-//! means that it requires the data
+    type Mutation {
+        createFriend(input: FriendInput): Friend
+        updateFriend(id: ID!, input: FriendInput): Friend
+    }
+`);
 
 export default schema;
--- scripts/updateSchema.js
+++ scripts/updateSchema.js
@@ -1,10 +1,12 @@
 import fs from 'fs';
 import path from 'path';
-import { schema } from '../data/schema';
+import {schema} from '../data/schema'
 import { printSchema } from 'graphql';
 
 const schemaPath = path.resolve(__dirname, '../data/schema.graphql');
-
+//
 fs.writeFileSync(schemaPath, printSchema(schema));
 
-console.log('Wrote' + schemaPath);
+//console.log('Wrote' + schemaPath);
+
+console.log('update Schema');
